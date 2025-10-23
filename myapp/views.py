7251607458393ff7d82fe0fb364c547b17c90a67
JoseDev7 <josefr7h@gmail.com>
2025-10-23 15:24:07 -0300

    modificacion de arcihvos

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect , get_object_or_404
 from .forms import CreateNewTask, CreateNewProject
 
 # Create your views here.
